bcb01a30e43d555c57a79d2d040ab4ef12f96fc4
Artem Orel <artem.o@avonow.com>
2022-12-02 17:34:26 +0200

    worked solution from Anna

--- finder.rb
+++ finder.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
 
+class Finder
+  def self.call(string)
+    result = []
+
+    string.chars.each_with_index do |char, i|
+      if char == string[i-1]
+        result[-1] = result.last << char
+      else
+        result << char
+      end
+    end
+
+    result.sort_by! {|substr| substr.length}
+    result.last
+  end
+end
+
